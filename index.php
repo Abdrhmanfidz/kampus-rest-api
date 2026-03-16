659f4531b6eb5466296830687fe76673569c8436
Abdul Rahman Hafidz <abdrahmanhafidz6@gmail.com>
2026-03-17 05:55:24 +0800

    Display MySQL environment variables in index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-echo "API Kampus REST API Aktif";
+echo "MYSQLHOST: " . $_ENV['MYSQLHOST'] . "<br>";
+echo "MYSQLUSER: " . $_ENV['MYSQLUSER'] . "<br>";
+echo "MYSQLPASSWORD: " . $_ENV['MYSQLPASSWORD'] . "<br>";
+echo "MYSQLDATABASE: " . $_ENV['MYSQLDATABASE'] . "<br>";
+echo "MYSQLPORT: " . $_ENV['MYSQLPORT'] . "<br>";
 ?>
