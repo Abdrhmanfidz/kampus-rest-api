249894f3158b59f68a203a655017a60ec1711d40
Abdul Rahman Hafidz <abdrahmanhafidz6@gmail.com>
2026-03-17 06:15:30 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,25 +1,25 @@
 <?php
 header("Content-Type: application/json");
-
 mysqli_report(MYSQLI_REPORT_OFF);
 
 $host = 'shuttle.proxy.rlwy.net';
-$user = 'root';
-$pass = 'loNOJCiUQGrPegvEOpNpVUKtVioRRwMn'; // ← ganti ini
-$db   = 'railway';
 $port = 35889;
+$user = 'root';
+$pass = 'loNOJCiUQGrPegvEOpNpVUKtVioRRwMn';
+$db   = 'railway';
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
-if ($conn->connect_error) {
+if ($conn->connect_errno) {
     die(json_encode([
-        "status" => "error",
-        "message" => "Koneksi gagal: " . $conn->connect_error
+        "status"  => "error",
+        "code"    => $conn->connect_errno,
+        "message" => $conn->connect_error
     ]));
 }
 
 echo json_encode([
-    "status" => "success", 
+    "status"  => "success",
     "message" => "Koneksi berhasil!"
 ]);
 ?>
